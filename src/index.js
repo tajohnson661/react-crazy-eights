432baf9cc1fd441a1b8af52ef31b2e106ad65f21
Tom Johnson <Tom@Toms-MacBook-Air.local>
2017-01-05 08:59:10 -0800

    setup for app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './components/Main';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 // Render the main component into the dom
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('app'));
