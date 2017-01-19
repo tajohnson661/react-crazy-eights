407455431b35154dc716064c81d27a06f4f760a7
Tom Johnson <tjohnson@toparstreet.com>
2017-01-19 10:10:54 -0800

    Add immutablejs library for immutable data structures in redux state and playing card data

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/Main';
 import reducers from './reducers';
 
+// Install dev tools for redux
+var Immutable = require('immutable');
+var installDevTools = require('immutable-devtools');
+installDevTools(Immutable);
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 // Render the main component into the dom
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())}>
     <App />
   </Provider>
   , document.getElementById('app'));
--- src/reducers/reducer_gameboard.js
+++ src/reducers/reducer_gameboard.js
@@ -1,98 +1,104 @@
-import {START_GAME, SET_MESSAGE, PLAYER_PLAYED, PLAYER_WINS, DEALER_DRAWS, PLAYER_DRAWS,
-        DEALER_PLAYED, DEALER_WINS, SET_SUIT} from '../actions/index';
+import {
+  START_GAME, SET_MESSAGE, PLAYER_PLAYED, PLAYER_WINS, DEALER_DRAWS, PLAYER_DRAWS,
+  DEALER_PLAYED, DEALER_WINS, SET_SUIT
+} from '../actions/index';
 import {initialDeck, shuffleDeck, dealCards, getCurrentSuitFromDiscard} from '../modules/Cards';
+import {List, Map} from 'immutable';
 
-const initialState = {
-  shuffledDeck: []
-  , playerHand: []
-  , dealerHand: []
-  , discardPile: []
+
+const initialState = Map({
+  shuffledDeck: List()
+  , playerHand: List()
+  , dealerHand: List()
+  , discardPile: List()
   , currentSuit: 'Hearts'
   , inProgress: false
   , message: 'Welcome to Crazy Eights'
   , showDialog: false
-};
+});
 
 export default function (state = initialState, action) {
+  let newState = Map({});
+
   switch (action.type) {
     case START_GAME:
       const allHands = dealCards(shuffleDeck(initialDeck()));
-
-      return {
-        ... state
-        , inProgress: true
-        , shuffledDeck: allHands.remainingDeck
-        , playerHand: allHands.playerHand
-        , dealerHand: allHands.dealerHand
-        , discardPile: allHands.discardPile
-        , currentSuit: getCurrentSuitFromDiscard(allHands.discardPile)
-        , message: 'To play, click on a card in your hand or draw'
-      };
+      newState = Map({
+          inProgress: true
+          , shuffledDeck: allHands.get('remainingDeck')
+          , playerHand: allHands.get('playerHand')
+          , dealerHand: allHands.get('dealerHand')
+          , discardPile: allHands.get('discardPile')
+          , currentSuit: getCurrentSuitFromDiscard(allHands.get('discardPile'))
+          , message: 'To play, click on a card in your hand or draw'
+        }
+      );
+      break;
 
     case SET_MESSAGE:
-      return {
-        ... state
-        , message: action.payload
-      };
+      newState = Map({
+        message: action.payload
+      });
+      break;
 
     case DEALER_DRAWS:
-      return {
-        ... state
-        , shuffledDeck: action.payload.shuffledDeck
+      newState = Map({
+        shuffledDeck: action.payload.shuffledDeck
         , dealerHand: action.payload.dealerHand
         , discardPile: action.payload.discardPile
-      };
+      });
+      break;
 
     case PLAYER_DRAWS:
-      return {
-        ... state
-        , shuffledDeck: action.payload.shuffledDeck
+      newState = Map({
+        shuffledDeck: action.payload.shuffledDeck
         , playerHand: action.payload.playerHand
-        , discardPile : action.payload.discardPile
+        , discardPile: action.payload.discardPile
         , message: action.payload.message
-      };
+      });
+      break;
 
     case PLAYER_PLAYED:
-      return {
-        ... state
-        , message: action.payload.message
+      newState = Map({
+        message: action.payload.message
         , playerHand: action.payload.playerHand
         , discardPile: action.payload.discardPile
         , currentSuit: action.payload.currentSuit
         , showDialog: action.payload.showDialog
-      };
+      });
+      break;
 
     case DEALER_PLAYED:
-      return {
-        ... state
-        , message: action.payload.message
+      newState = Map({
+        message: action.payload.message
         , dealerHand: action.payload.dealerHand
         , discardPile: action.payload.discardPile
         , currentSuit: action.payload.currentSuit
-      };
+      });
+      break;
 
     case PLAYER_WINS:
-      return {
-        ... state
-        , message: 'You win!!!'
+      newState = Map({
+        message: 'You win!!!'
         , inProgress: false
-      };
+      });
+      break;
 
     case DEALER_WINS:
-      return {
-        ... state
-        , message: 'Dealer wins!'
+      newState = Map({
+        message: 'Dealer wins!'
         , inProgress: false
-      };
+      });
+      break;
 
     case SET_SUIT:
-      return {
-        ... state
-        , currentSuit: action.payload
+      newState = Map({
+        currentSuit: action.payload
         , showDialog: false
-      };
+      });
+      break;
   }
-  return state;
+  return state.merge(newState);
 }
 
 
