df3eba184081b7ab56e65b399dfd99d4bf842dbf
Tom Johnson <Tom@Toms-MacBook-Air.local>
2017-01-25 09:16:39 -0800

    use material-ui for dialog

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,14 @@
 require('styles/App.css');
 import React from 'react';
 import GameBoard from '../containers/GameBoard';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <GameBoard />
+      <MuiThemeProvider>
+        <GameBoard />
+      </MuiThemeProvider>
     );
   }
 }
